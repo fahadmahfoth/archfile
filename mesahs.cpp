ce8dd6ccee33b9bce7f319700c26f9650a829963
Fahad Mahfoth <35464579+fahadmahfoth@users.noreply.github.com>
2021-04-26 05:17:21 +0300

    add file to github

---
+++ mesahs.cpp
@@ -0,0 +1,43 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+        double a  , b ;
+
+while(true){
+    
+    cin >> a ;
+    cin >> b;
+    while(a - b >=  30) {
+
+
+
+double x  = b-a ;
+
+
+
+
+double xx = 30 /x ;
+
+double xxx = 30 - b ;
+
+double result = xx * xxx ;
+
+cout <<"result is " << result << endl;
+b+= 29 ;
+
+
+
+
+    
+}
+}
+
+
+
+}
+
